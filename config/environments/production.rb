12b6870a79f1808b09558a0ff88d88c1cf032c66
William Fish <william.fish@mydrivesolutions.com>
2016-09-23 15:20:07 +0100

    Configure smtp using gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
+    user_name: ENV["EMAIL"],
+    password: ENV["EMAIL_PASSWORD"],
+    authentication: "plain",
     enable_starttls_auto: true
   }
 
